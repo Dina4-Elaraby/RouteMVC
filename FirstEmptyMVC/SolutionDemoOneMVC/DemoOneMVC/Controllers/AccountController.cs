ec944425acc9a5224016e4b73eb7a1832010f9fa
dinaelaraby <dinaalaraby9503@gmail.com>
2025-04-11 18:17:54 +0200

    Assignment_2:Routing&Actions&Views

---
+++ FirstEmptyMVC/SolutionDemoOneMVC/DemoOneMVC/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoOneMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        //BaseUrl/Account/SignIn
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        //BaseUrl/Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC Demo/Demo.DAL/Data/Repositores/Classs/DepartmentRepo.cs
@@ -0,0 +1,43 @@
+﻿using Demo.DAL.Data.Repositores.Interfaces;
+using Demo.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Data.Repositores.Classs
+{
+    internal class DepartmentRepo : IDepartmentRepo
+    {
+        private AppDbContext dbContext; //field ,default value => null
+        public DepartmentRepo()
+        {
+
+        }
+        public int Delete(Department row)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Department GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Department row)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Department dept)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
